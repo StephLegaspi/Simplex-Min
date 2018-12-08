0f6dac6da5ae828c90b5977e0e022e98a8d7de43
StephLegaspi <sylegaspi@up.edu.ph>
2018-12-08 20:08:07 +0800

    unfinished

--- simplex.r
+++ simplex.r
@@ -1,15 +1,19 @@
 
 GetPivotColumn <- function(last_row, col_tableu){
+  
   for(j in 1:col_tableu){
     if(last_row[j] < 0 ){
       max_col = j
+      #print(max_col)
       break
     }
   }
   
   for(i in 1:col_tableu){
     if(last_row[i] < 0){
-      if(abs(last_row[i]) > abs(last_row[max_col])){ 
+      #print(last_row[i])
+      #print(last_row[max_col])
+      if(abs(last_row[i]) > abs(last_row[max_col]) || abs(last_row[i]) == abs(last_row[max_col]) ){ 
         max_col = i 
       }
     }
@@ -18,7 +22,7 @@ GetPivotColumn <- function(last_row, col_tableu){
 }
 
 CheckNegative <- function(last_row, col_tableu){
-  for(i in 1:col_tableu){
+  for(i in 1:(col_tableu-2)){
     if(last_row[i] < 0) { return(TRUE) }
   }
   return(FALSE)
@@ -28,7 +32,7 @@ GetTestRatio <- function(pivot_col, RHS, row_tableu){
   pivot_col <- pivot_col[-row_tableu]
   RHS <- RHS[-row_tableu]
   test_ratio = RHS / pivot_col
-  #print(test_ratio)
+  print(test_ratio)
   
   for(j in 1:(row_tableu-1)){
     if(test_ratio[j] > 0){
@@ -131,21 +135,83 @@ preprocessMatrix <- function(init_matrix){
   return(init_tableu)
 }
 
-checkNegative2 <- function(mod_tableu){
+checkNegative2 <- function(mod_tableu, r, c){
   if(mod_tableu["s4", "s4"]<0 || mod_tableu["s5", "s5"]<0 || mod_tableu["s6", "s6"]<0 || mod_tableu["s7", "s7"]<0 || mod_tableu["s8", "s8"]<0){
+  #if(mod_tableu["s1", "s1"]<0 || mod_tableu["s2", "s2"]<0){
     return(TRUE)
   }else{ return(FALSE) }
+  #for(row in 1:r){
+  #  for(col in 1:c){
+  #    if(mod_tableu[row, col] < 0){return(TRUE)}
+    #}
+  #}
+  #return(FALSE)
+}
+
+GetNeg <- function(mod_tableu, row, col){
+  for(i in 0:4){
+    if(mod_tableu[row+i, col+i] == -1){
+      return(list(r=row+i, c=col+i))
+      break
+    }
+  }
+  
+  #for(i in 1:row){
+   # for(j in 1:col){
+    #  if(mod_tableu[i, j] == -1){
+        #print(mod_tableu[i, j])
+     #   return(list(r=i, c=j))
+      #}
+  #  }
+  #}
+}
+
+GetPivotColumn2 <- function(mod_tableu, neg_row, last_col){
+  for(col in 1:last_col){
+    if(mod_tableu[neg_row, col] != 0 && mod_tableu[neg_row, col] > 0){ return(col) }
+  }
 }
 
 removeNegative <- function(mod_tableu){
+  r = nrow(mod_tableu)
+  c = ncol(mod_tableu)
   start_col = 19
   last_col = 23
   start_row = 4
   last_row = 8
+  new_start_col = 0
   
-  if_neg = checkNegative2(mod_tableu)
-  print(if_neg)
-  #pos_index = getFarthestPos(mod_tableu, curr_row)
+  if_neg = checkNegative2(mod_tableu, r-1, c-1)
+  #while(if_neg == TRUE){
+  for(ctr in 1:4){
+    neg_row = GetNeg(mod_tableu, start_row, start_col)
+    #print(neg_row)
+    
+    pivot_col_index = GetPivotColumn2(mod_tableu, neg_row$r, (neg_row$c - 1))
+    print(pivot_col_index)
+    pivot_col = mod_tableu[,pivot_col_index]
+    #print(pivot_col)
+    RHS = mod_tableu[, c]
+    
+    pivot_row_index = GetTestRatio(pivot_col, RHS, r)
+    print(pivot_row_index)
+    pivot_element = mod_tableu[pivot_row_index, pivot_col_index]
+    pivot_row = mod_tableu[pivot_row_index, ] / pivot_element
+    mod_tableu[pivot_row_index,] = pivot_row
+    
+    for(i in 1:r){
+      if(i != pivot_row_index){
+        val_to_zero = mod_tableu[i, pivot_col_index]
+        temp = ComputeTemp(val_to_zero, pivot_row)
+        new_row = ComputeNewRow(temp, mod_tableu[i, ])
+        mod_tableu[i, ] = new_row
+      }
+    }
+    last_row = mod_tableu[r, ]
+    if_neg = checkNegative2(mod_tableu, start_row, start_col)
+    print(mod_tableu)
+  }
+  
 }
 
 SimplexMethod <- function(tableu){
@@ -177,7 +243,7 @@ SimplexMethod <- function(tableu){
     }
     last_row = tableu[row_tableu, ]
     neg = CheckNegative(last_row, col_tableu)
-    #print(tableu)
+    print(tableu)
   }
   values = GetValues(tableu,row_tableu, col_tableu)
   return(tableu)
@@ -225,10 +291,27 @@ df2 = matrix(c(310, 10, 8, 6, 5, 4,
       byrow = TRUE)
 
 #print(init_tableu)
-#res = SimplexMethod(init_tableu)
-#print(res)
+
+sample_tableu = matrix(
+  c(2, 3, 6, 1, 0, 0, 60,
+    1, 4, 5, 0, -1, 0, 40,
+    3, 2, 3, 0, 0, 1, 0),
+  nrow = 3,
+  ncol = 7,
+  byrow = TRUE
+)
+
+dimnames(sample_tableu) = list(
+  c("s1", "s2", "Z"),
+  c("x", "y", "z", "s1", "s2", "Z", "RHS")
+)
+
 init_tableu = preprocessMatrix(df2)
 print(init_tableu)
+#res = SimplexMethod(init_tableu)
+#print(res)
+#print(df2)
+
 removeNegative(init_tableu)
 
 
